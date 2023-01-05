a9a21f50228f2f4e1e4227290aea898e83e6fe0f
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2023-01-05 10:59:43 +0100

    knapp

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const knappMenyEl = document.getElementById("menyKnapp");
 const menyEl = document.getElementById("navBar")
 
+let mybutton = document.getElementById("topBtn");
 
 function visaMeny() {
 
     knappMenyEl.style.display = "none";
     menyEl.style.display = "flex";
 }
+window.onscroll = function () { scrollFunction() };
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 knappMenyEl.addEventListener('click', visaMeny);
+mybutton.addEventListener('click', topFunction);
